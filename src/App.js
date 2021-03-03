4f8938ea2854bea78f9b8d4086e61b1e63ba26fc
Jessica Leverett <Jessak1202@gmail.com>
2021-03-03 05:42:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All rights are reserved. 2021 &copy;
+      </footer>
     </div>
   );
 }
