484af92be015455889c8c1efd79aead54bc05bec
Jessica Leverett <Jessak1202@gmail.com>
2021-03-03 05:47:37 -0600

    first committ

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
-        <a href="/">React Shopping Cart</a>
+        <a href="/">React Ecommerce Shopping Cart</a>
       </header>
       <main>
         Product List
